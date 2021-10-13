0ccdc3aab09c8634140ff8f80869f408f1673062
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-13 12:36:49 +1100

    styling update

--- generator-one/src/App.js
+++ generator-one/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./CSS/index.css"
 
 const App = () => {
     return (
-        <div>
-          <h1> test</h1>  
-        </div>
+    <main>
+        <h1> Why you should Learn JavaScript Text Generator?</h1>
+    </main>
     )
 }
 
