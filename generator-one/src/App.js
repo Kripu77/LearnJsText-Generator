fd07e8ad80bdd1dc56c5c95c935b092967828b75
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-13 13:02:21 +1100

    functrional state update

--- generator-one/src/App.js
+++ generator-one/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from "./Components/data"
+import {ToastContainer, toast} from "react-toastify"
+  import 'react-toastify/dist/ReactToastify.css';
 import "./CSS/index.css"
 
+
 const App = () => {
+    const [count, setCount] = useState(0);
+    const [para, setPara] = useState([])
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+       setPara([...para, data[count]])
+        
+
+    }
     return (
     <main>
         <h1> Why you should Learn JavaScript Text Generator?</h1>
+        <section className='form-container'>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='paragraphs'>Paragraphs:</label>
+                <input type="number" id="paragraphs" name="paragraphs" value={count} onChange={()=>setCount(count+1)}></input>
+                <button className="btn" type='submit'  >Generate</button>
+            </form>
+            
+
+        </section>
+        <section>
+            {para.map((value, index)=>{
+                return <p className='info-dislay' key={index}>{value}</p>
+            })}
+        </section>
     </main>
+    
     )
 }
 
